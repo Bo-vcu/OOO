e768585225388d5a7957d5ef71fc60b3242d248e
Gert Aesloos <gert.aesloos@student.ucll.be>
2022-10-03 16:09:43 +0200

    main methode juist

--- Encoder/src/ui/Main.java
+++ Encoder/src/ui/Main.java
@@ -6,12 +6,7 @@ import domain.Spiegeling;
 
 public class Main {
     public static void main(String[] args) {
-        String a= "hello world";
-        Spiegeling spiegeling=new Spiegeling();
-        Caesar caesar=new Caesar();
-        String b=caesar.encode(a);
-
-        System.out.println(caesar.encode(a));
-        System.out.println(caesar.decode(b));
+        EncoderUI encoderUI = new EncoderUI();
+        encoderUI.root();
     }
 }
