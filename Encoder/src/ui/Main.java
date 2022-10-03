1c818e395d0819f87c06e640ab4a62b1e60f7b1f
Gert Aesloos <gert.aesloos@student.ucll.be>
2022-10-03 16:23:26 +0200

    main methode juist!!!

--- Encoder/src/ui/Main.java
+++ Encoder/src/ui/Main.java
@@ -3,10 +3,18 @@ package ui;
 
 import domain.Caesar;
 import domain.Spiegeling;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+    EncoderUI encoderUI=new EncoderUI();
     public static void main(String[] args) {
-        EncoderUI encoderUI = new EncoderUI();
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
         encoderUI.root();
     }
 }
